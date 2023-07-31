c9788a8bdf66e66de693f11d191ea7ea88abda63
Hitoshi Manabe <int.toshi13@gmail.com>
2023-07-31 21:05:33 +0900

    fix

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class my_build_ext(build_ext):
 
     def finalize_options(self):
         build_ext.finalize_options(self)
-        __builtins__.__NUMPY_SETUP__ = False
+        # __builtins__.__NUMPY_SETUP__ = False
         import numpy
 
         self.include_dirs.append(numpy.get_include())
